070020a94ae9914158233c448247658df198088c
Leandro Oliveira <leandro@agapsys.com>
2015-12-19 18:15:48 -0300

    Refactoring + new features progress

--- src/main/java/com/agapsys/utils/console/TableBuilder.java
+++ src/main/java/com/agapsys/utils/console/TableBuilder.java
@@ -15,64 +15,53 @@
  */
 package com.agapsys.utils.console;
 
+import com.agapsys.utils.console.RowBuilder.Column;
+import com.agapsys.utils.console.TableDefinitionBuilder.ColumnAlignment;
 import com.agapsys.utils.console.TableDefinitionBuilder.ColumnDefinition;
 import java.util.LinkedList;
 import java.util.List;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.WordUtils;
 
 public class TableBuilder {
-
-	// CLASS SCOPE =============================================================
-	private static String wrap(String srcText, int width, boolean recursive) {
-		if (srcText.equals("\n"))
-			return srcText;
-					
-		width = width - 1;
-		String[] paragraphs = srcText.split("\n");
-		
-		for (int i = 0; i < paragraphs.length; i++) {
-			paragraphs[i] = paragraphs[i].trim();
-		}
-		
-		StringBuilder totalText = new StringBuilder();
-		
-		// Quebra as linhas dentro do parágrafo:
-		for (int i = 0; i < paragraphs.length; i++) {
-			StringBuilder sb = new StringBuilder(paragraphs[i]);
-
-			int j = 0;
-			while (j + width < sb.length() && (j = sb.lastIndexOf(" ", j + width)) != -1) {
-			    sb.replace(j, j + 1, "\n");
-			}
-
-			// trim lines
-			String[] lines = sb.toString().split("\n");
-			StringBuilder sbFinal = new StringBuilder();
-			for (int k = 0; k < lines.length; k++) {
-				lines[k] = lines[k].trim();
-				sbFinal.append(lines[k]);
-				if (k < (lines.length - 1))
-					sbFinal.append("\n");
-			}
-			
-			if (i < (paragraphs.length - 1))
-				sbFinal.append("\n");
-			
-			totalText.append(sbFinal.toString());
-		}
-		
-		return totalText.toString();
-	}
-	
-	public static String wrap(String str, int width) {
-		return wrap(str, width, false);
-	}
 	
 	public static void main(String...args) {
-		Console.println(ConsoleColor.MAGENTA, wrap("Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.", 50));
-		System.out.println("");
-		Console.println(ConsoleColor.MAGENTA, wrap("Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and\n\n limitations under the License.", 50));
+		TableBuilder tb = new TableBuilder();
+		tb.definition()
+			.col(2, ColumnAlignment.LEFT)
+			.col(11, ColumnAlignment.LEFT)
+			.col(30, ColumnAlignment.LEFT)
+			.col(2, ColumnAlignment.LEFT)
+		.end()
+		.row()
+			.col(ConsoleColor.CYAN, "id")
+			.col(ConsoleColor.CYAN, " date")
+			.col(ConsoleColor.CYAN, " Name")
+			.col(ConsoleColor.CYAN, " G")
+		.end()
+		.row()
+			.col("1")
+			.col(" abcd-ef-gh ")
+			.col(" aaaaa bbbbbb cccccc dd eeeeeeee f gggg hhhh i jjjjj kk lll mm nnnn")
+			.col(ConsoleColor.YELLOW, "f")
+		.end()
+		//.row().end() // test this
+		.row()
+			.col("2")
+			.col(" ijkl-mn-op ")
+			.col(" ooooooo pppp qqqqqq rr ssssssss")
+			.col(ConsoleColor.YELLOW, "m")
+		.end()
+		//.row().end() // test this
+		.row()
+			.col("3")
+			.col(" qrst-uv-wx ")
+			.col(" tttttt uuuuuu vv wwwwwwww")
+			.col(ConsoleColor.YELLOW, "m")
+		.end();
+		System.out.println(tb.toString());
 	}
-	// =========================================================================
+
 	public TableDefinitionBuilder tdb = null;
 	public final List<RowBuilder> rows = new LinkedList<>();
 
@@ -91,15 +80,73 @@ public class TableBuilder {
 		return rb;
 	}
 
-	public void println() {
-		if (tdb == null) {
+	private String getCell(ConsoleColor fgColor, ColumnAlignment alignment, int width, String value) {
+		String baseStr = String.format("%" + (alignment == ColumnAlignment.LEFT ? "-" : "") + width + "s", value);
+		return Console.getOutputString(fgColor, baseStr);
+	}
+	
+	@Override
+	public String toString() {
+		if (tdb == null)
 			throw new IllegalStateException("Table definition is not set");
+		
+		int colNum = tdb.getColumnDefinitions().size();
+		int rowNum = 0;
+		
+		for (RowBuilder row : rows) {
+			rowNum++;
+			for (int j = 0; j < row.getColumns().size(); j++) {
+				Column col = row.getColumns().get(j);
+				ColumnDefinition colDef = tdb.getColumnDefinitions().get(j);
+				
+				rowNum += StringUtils.countMatches(WordUtils.wrap(col.value, colDef.width, "\n", true), "\n");
+			}
 		}
-
-		List<ColumnDefinition> colDefs = tdb.getColumnDefinitions();
-//		for (RowBuilder row : rows) {
-//			int i = 0;
-//			for ()
-//		}
+		
+		String[][] strArray = new String[rowNum][colNum];
+		
+		int i = 0;
+		for (RowBuilder row : rows) {
+			int k = 0;
+			int maxK = 0;
+			for (int j = 0; j < colNum; j++) {
+				ColumnDefinition  colDef = tdb.getColumnDefinitions().get(j);
+				Column col = row.getColumns().get(j);
+				String[] cRows = WordUtils.wrap(col.value, colDef.width).split("\n");
+				k = 0;
+				for (String cRow : cRows) {
+					String cRowStr = WordUtils.wrap(cRow, colDef.width);
+					strArray[i + k][j] = getCell(col.fgColor, colDef.alignment, colDef.width, cRowStr);
+					k++;
+				}
+				if (k > maxK)
+					maxK = k;
+			}
+			i += maxK;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (i = 0; i < rowNum; i++) {
+			StringBuilder rowFormat = new StringBuilder();
+			
+			for (ColumnDefinition colDef : tdb.getColumnDefinitions()) {
+				rowFormat.append("%").append(colDef.alignment == ColumnAlignment.LEFT ? "-" : "").append(colDef.width).append("s");
+			}
+			
+			List<String> colValues = new LinkedList<>();
+			for (int j = 0; j < colNum; j++) {
+				String colValue = strArray[i][j];
+				if (colValue == null)
+					colValue = "";
+				
+				colValues.add(colValue);
+			}
+			
+			sb.append(String.format(rowFormat.toString(), colValues.toArray()));
+			if (i < rowNum - 1)
+				sb.append("\n");
+		}
+		
+		return sb.toString();
 	}
 }
