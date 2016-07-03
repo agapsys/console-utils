02245bc6878e414e5fa990e28d746faa58b4fdeb
Leandro Oliveira <ljbo.82@gmail.com>
2016-07-03 13:22:24 -0300

    Updated copyright

--- src/main/java/com/agapsys/utils/console/args/ActionParser.java
+++ src/main/java/com/agapsys/utils/console/args/ActionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Agapsys Tecnologia Ltda-ME.
+ * Copyright 2016 Leandro José Britto de Oliveira.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
